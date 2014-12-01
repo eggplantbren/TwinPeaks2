d385c46df262bc05bdeba4db0ac0370f9f38d9d8
Brendon Brewer <brendon.brewer@gmail.com>
2014-12-01 19:57:25 +1300

    More general, better comments

--- biased_sampling.jl
+++ biased_sampling.jl
@@ -5,17 +5,22 @@ include("Utils.jl")
 # Seed rng
 srand(0)
 
-x = rand(10000)
-x = append!(x, 0.5*rand(10000))
-x = append!(x, 0.25*rand(10000))
+# Sample from three distributions
+x = rand(1000)
+x = append!(x, 0.5*rand(1000))
+x = append!(x, 0.25*rand(1000))
+n = [1000, 1000, 1000]	# Number of points from each distribution
+f = n/sum(n)		# Fraction of points from each distribution
+num_distributions = 3
 
+# True upper limits of the distributions
 limits = [1., 0.5, 0.25]
 
-# Using Equation 8 of Cameron et al
 # Initial guesses of normalising constants
 logZ = [0., 0., 0.]
 
 # Function to do one iteration of logZ[k]
+# Using Equation 8 of Cameron et al
 function update!(logZ::Array{Float64, 1}, k::Int64)
   terms = zeros(length(x))
   for i in 1:length(x)
@@ -27,10 +32,11 @@ function update!(logZ::Array{Float64, 1}, k::Int64)
   logZ[k] = logsumexp(terms) - log(length(x))
 end
 
+# Evaluate the mixture
 function logp_mix(logZ::Array{Float64, 1}, x::Float64)
-  terms = zeros(3)
-  for i in 1:3
-    terms[i] = log(1./3) - logZ[i]
+  terms = zeros(num_distributions)
+  for i in 1:num_distributions
+    terms[i] = log(f[i]) - logZ[i]
     if x > limits[i]
       terms[i] = -1E300
     end
